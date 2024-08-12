1fe0d3f852aa907606303c7af3b447607206ab9f
Xkill3rXx <lucas.carvalho1999@hotmail.com>
2024-08-12 19:54:16 -0300

    agora todos da index estão igual a home

--- functions.js
+++ functions.js
@@ -1,11 +1,1 @@
-//adicionando evento do click no elemento de id video
-document.getElementById('videos').addEventListener('click', function(){
-    document.getElementById('vid').scrollIntoView({
-        behavior:"smooth"
-    })
-})
-
-//adicionando evento do click no elemento de id "youtube"
-document.getElementById('youtube').addEventListener('click', function(){
-    window.open("https://youtube.com/@olucaocarvalho?si=QmZYDwHK65o3VqgZ")
-})
+// https://youtube.com/@olucaocarvalho?si=QmZYDwHK65o3VqgZAgAB
